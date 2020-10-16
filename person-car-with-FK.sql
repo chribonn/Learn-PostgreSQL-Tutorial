17dbeb8fcf3c81254eac8c0ec692bc0c5e0bb577
Alan C Bonnici <chribonn@gmail.com>
2020-10-16 15:24:26 +0200

    UUID table modification

--- person-car-with-FK.sql
+++ person-car-with-FK.sql
@@ -1,19 +1,19 @@
 CREATE TABLE car (
-	id BIGSERIAL NOT NULL PRIMARY KEY,
+	car_uid UUID  NOT NULL PRIMARY KEY,
 	make VARCHAR(100) NOT NULL,
 	model VARCHAR(100) NOT NULL,
-	price NUMERIC(19, 2) NOT NULL
+	price NUMERIC(19, 2) NOT NULL CHECK (price > 0)
 );
 
 CREATE TABLE person (
-	id BIGSERIAL NOT NULL PRIMARY KEY,
+	person_uid UUID NOT NULL PRIMARY KEY,
 	first_name VARCHAR(50) NOT NULL,
 	last_name VARCHAR(50) NOT NULL,
 	gender VARCHAR(7) NOT NULL,
-	email VARCHAR(100),
+	email VARCHAR(100) CHECK (email ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'),
 	date_of_birth DATE NOT NULL,
 	country_of_birth VARCHAR(50) NOT NULL,
-	car_id BIGINT REFERENCES car(id),
+	car_uid UUID REFERENCES car(car_uid),
 	UNIQUE(car_id)
 );
 
